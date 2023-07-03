ada41352d053493de846c6b509e763c382abd118
prnv <0227prnvm@gmail.com>
2023-07-03 22:38:38 +0530

    p

---
+++ string_ques.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class string_ques {
+    public static int getseries(int n ) {
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a=1;
+        int b=1;
+
+        for (int i = 1; i <n; i++) {
+            if(n%2!=0){
+                if(i==1){
+                a=1;
+                }
+                a = a*2;
+            }
+            else {
+                if(i==2)
+                    b=1;
+                b = b*3;
+            }
+
+
+        }
+    }
+}
